458179994174b58a652cde7f257e95f01174defa
Matias Mirotta <matias.mirotta@bp-4.com>
2020-04-09 21:29:30 -0300

    Input y Output

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,7 +7,7 @@ import { Usuario } from '../../clases/usuario';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  title = 'Administrador';
   usuario = new Usuario();
   listadoPrincipal: Usuario[];
   usuarioSeleccionado: Usuario;
