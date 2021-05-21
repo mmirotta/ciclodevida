fa0f4135a4c987ccb6393b61b12a629fdee458ad
Matias Mirotta <matias.mirotta@bp-4.com>
2021-05-20 22:01:23 -0300

    UPDATE

--- src/app/componentes/grilla-paises/grilla-paises.component.spec.ts
+++ src/app/componentes/grilla-paises/grilla-paises.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GrillaPaisesComponent } from './grilla-paises.component';
 
@@ -6,7 +6,7 @@ describe('GrillaPaisesComponent', () => {
   let component: GrillaPaisesComponent;
   let fixture: ComponentFixture<GrillaPaisesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GrillaPaisesComponent ]
     })
